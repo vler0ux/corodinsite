150e44f3ce9f5e9576628c1cddb5f36c512798f4
vlerOux <vleroux9738@gmail.com>
2025-06-19 15:51:34 +0200

    get started husky

--- src/tests/resultat.test.ts
+++ src/tests/resultat.test.ts
@@ -2,9 +2,6 @@ import { determineHeure} from '../corodinsite';
 import { expect, describe, it } from 'vitest';
 
 describe('determineHeure', () => {
-    it('should return 0 when 1 + 1+1 because 1+1+rien -2', () => {
-        expect(determineHeure(1, 1,1)).toBe(0);
-    });
 
     it('should return 6 when 2+2+2', () => {
         expect(determineHeure(2, 2,2)).toBe(6);
@@ -14,9 +11,7 @@ describe('determineHeure', () => {
         expect(determineHeure(2, 2, 1)).toBe(4);
     });
 
-    it('should return 0 when 1+1+2 because 1+1+rien -2', () => {
-        expect(determineHeure(1, 1,2)).toBe(0);
-    });
+
 
     it('should return 5 because 1+2+ 1*2 -2 +2 ', () => {
         expect(determineHeure(1, 2,1)).toBe(5);
