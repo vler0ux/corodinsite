d44c13131cbe0dc511fd1ce4f41d772b7b5863cf
vlerOux <vleroux9738@gmail.com>
2025-06-19 15:44:48 +0200

    test unitaire 3 sur 2 ok

--- src/corodinsite.ts
+++ src/corodinsite.ts
@@ -1,32 +1,33 @@
-export function determineHeure( cadranLune : 1|2, cadranSoleil:1|2, cadranTerre:1|2){
+export function determineHeure( cadranLune : 1|2, cadranSoleil:1|2, cadranTerre:1|2):number{
 
+    let valeurTerre = cadranTerre;
+    let pouvoirTerre = cadranTerre;
+    let ignorerTerre = false;
 
-let resultat= cadranLune + cadranSoleil + cadranTerre;
-let utiliseTerre = true;
-
-
- if (cadranLune === 1) {
-    resultat -= 2;
-  } else if (cadranLune === 2) {
-    resultat = resultat / 2;
-  }
-
-
-if (cadranSoleil === 1) {
-    utiliseTerre = false;
-  } else if (cadranSoleil === 2) {
-    cadranTerre = cadranTerre * 2;
-  }
-
-
- if (utiliseTerre) {
-    if (cadranTerre === 1) {
-      resultat += 2;
-    } else if (cadranTerre === 2) {
-      resultat = 6;
-    }
-  }
-  return resultat;
+    if (cadranSoleil === 1) {
+        ignorerTerre = true;
+    }    else if(cadranSoleil === 2) {
+    valeurTerre = cadranTerre * 2;
 }
 
-console.log(determineHeure(2, 2, 1));
+
+let resultat = cadranLune + cadranSoleil + valeurTerre;
+
+
+if(!ignorerTerre){
+    if (pouvoirTerre === 1) {
+    resultat += 2;
+    } else if (pouvoirTerre === 2) {
+    return 6;
+    }
+}
+
+
+    if (cadranLune === 1) {
+    resultat -= 2;
+    } else if (cadranLune === 2) {
+    resultat = Math.floor(resultat / 2);
+    }
+
+    return resultat;
+}
--- src/tests/resultat.test.ts
+++ src/tests/resultat.test.ts
@@ -7,11 +7,11 @@ describe('determineHeure', () => {
     });
 
     it('should return 6 when 2+2+2', () => {
-        expect(() => determineHeure(2, 2,2)).toBe(6);
+        expect(determineHeure(2, 2,2)).toBe(6);
     });
 
     it('should return 4, because (2+2+ 1*2 +2)/2, so 8/2=4 ', () => {
-        expect(() => determineHeure(2, 2, 1)).toBe(4);
+        expect(determineHeure(2, 2, 1)).toBe(4);
     });
 
     it('should return 0 when 1+1+2 because 1+1+rien -2', () => {
